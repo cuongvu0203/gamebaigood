f2651f6ed50a3fd6cfac6e5533cb893eaba3ec00
cuongvu <cuongvu@quangich.com>
2023-06-19 16:44:49 +0700

    first commit

---
+++ public/web/js/strings.js
@@ -0,0 +1,30 @@
+var download_link_ios_http              = "http://127.0.0.1/";
+var download_link_android_http          = "http://127.0.0.1/";
+var download_link_ios                   = "http://127.0.0.1/";
+var download_link_android               = "http://127.0.0.1/";
+
+var download_link_http_1                = "http://127.0.0.1/";
+var download_link_http_2                = "http://127.0.0.1/";
+var download_link_http_3                = "http://127.0.0.1/";
+var download_link_https_1               = "http://127.0.0.1/";
+var download_link_https_2               = "http://127.0.0.1/";
+var download_link_https_3               = "http://127.0.0.1/";
+
+var download_otp_android_url = "http://127.0.0.1/downloads/NO86.apk";
+var download_otp_ios_url = "itms-services://?action=download-manifest&amp;url=http://127.0.0.1/downloads/DUBAI.plist";
+
+var download_ipa_ios_url = "itms-services://?action=download-manifest&amp;url=http://127.0.0.1/downloads/DUBAI.plist";
+// var download_app_store_ios_url = "https://short.vin/luxyappstore";
+var download_app_store_ios_url = "itms-services://?action=download-manifest&amp;url=http://127.0.0.1/downloads/DUBAI.plist";
+
+var download_apk_android_url = "http://127.0.0.1/downloads/NO86.apk";
+var download_ch_play_android_url = "http://127.0.0.1/downloads/NO86.apk";
+
+var facebook_fanpage_url = "https://www.facebook.com/no86.club/";
+var facebook_group_url = "https://www.facebook.com/groups/205149784445237/";
+
+var vpn_chrome_url = "https://chrome.google.com/webstore/detail/touch-vpn-secure-and-unli/bihmplhobchoageeokmgbdihknkjbknd?hl=en";
+var vpn_firefox_url = "https://addons.mozilla.org/en-US/firefox/addon/touch-vpn/";
+var vpn_ios_url = "https://apps.apple.com/us/app/touch-vpn-unlimited-proxy/id991744383"
+var vpn_android_url = "https://play.google.com/store/apps/details?id=com.cloudflare.onedotonedotonedotone&hl=en"
+var nohu_url = "http://127.0.0.1/";
